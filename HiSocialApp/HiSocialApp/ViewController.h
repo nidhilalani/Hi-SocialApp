e5e8ebe2f7c1238bb2fd24e45c67432731e04722
indianic <indianic@ind601-2.local>
2015-07-16 19:47:44 +0530

    Initial commit

---
+++ HiSocialApp/HiSocialApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HiSocialApp
+//
+//  Created by indianic on 16/07/15.
+//  Copyright (c) 2015 indianic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HiSocialApp/HiSocialApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HiSocialApp
+//
+//  Created by indianic on 16/07/15.
+//  Copyright (c) 2015 indianic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
